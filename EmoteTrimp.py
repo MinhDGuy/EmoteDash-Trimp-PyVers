76c4fbf7d98f467792d46448a2643030e11a32d3
MinhDevGuy <104999039+MinhDGuy@users.noreply.github.com>
2024-01-07 09:02:10 +0700

    Update EmoteTrimp.py

--- EmoteTrimp.py
+++ EmoteTrimp.py
@@ -2,7 +2,7 @@ import ctypes
 
 def main():
     username = input("Enter Your Roblox Username(It only works when you enter the correct account name you are playing Roblox with): ")
-    if username != "Tiramiwal":
+    if username != "TW":
         filePath = r"source\EmoteTrimp.ahk"
         result = ctypes.windll.shell32.ShellExecuteW(None, "open", filePath, None, None, 1)
         if result > 32:
